bc7ee72cd554e686fd83b9bc2b9ea2c3b574fad8
Kevin T <124527625+K-Tsan@users.noreply.github.com>
2024-11-15 14:22:59 +0000

    Added menu

---
+++ Maths-Game/Menu.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Maths_Game
+{
+    public class Menu
+    {
+        public int runMenu()
+        {
+            Console.Clear();
+            do
+            {
+                Console.WriteLine("Please choose an option:\n" +
+                "1.  Addition\n" +
+                "2.  Subtraction\n" +
+                "3.  Multiplication\n" +
+                "4.  Division\n" +
+                "5.  All\n" +
+                "6.  Exit\n");
+
+                var input = Console.ReadLine();
+                int value;
+                if (int.TryParse(input, out value))
+                {
+                    if (value > 0 && value <= 6)
+                    {
+                        return value;
+                    }
+
+                }
+                Console.Clear();
+                Console.WriteLine("Please enter a valid option.");
+            } while (true);
+        }
+    }
+}
--- Maths-Game/Program.cs
+++ Maths-Game/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.ComponentModel.Design;
+using Maths_Game;
+
+namespace Maths_Game
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var menu = new Menu();
+            var option = menu.runMenu();
+            Console.WriteLine($"{option} has been picked.");
+            Console.WriteLine("Press any button to close");
+            Console.ReadKey();
+
+        }
+    }
+}
